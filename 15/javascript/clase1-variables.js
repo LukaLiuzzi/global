1f4850ffbf718c2038aadabea09e56e9f618b480
Luka <lukaliuzzidev@gmail.com>
2024-01-08 20:27:46 -0300

    Add JavaScript variables and index.html file

---
+++ 15/javascript/clase1-variables.js
@@ -0,0 +1,67 @@
+// Comentarios
+// Comentario de una sola linea
+/*
+Comentario de varias lineas
+Otra linea
+*/
+
+// Variables => Es un espacio en memoria que se reserva para almacenar un valor
+// var => Es una palabra reservada para declarar una variable => Ya no se usa!! Esta deprecada.
+// let => Es una palabra reservada para declarar una variable => Nos sirve para declarar variables que van a cambiar su valor a lo largo de la ejecucion del programa.
+// const => Es una palabra reservada para declarar una variable => Nos sirve para declarar variables que NO van a cambiar su valor a lo largo de la ejecucion del programa.
+
+// Declaracion de una variable
+let saldo = 0 // Saldo inicial
+// Gabriel => $100 => Luka
+// Reasignacion de una variable
+saldo = 100 // Saldo actual
+saldo = 200 // Saldo actual
+let direccion
+
+direccion = "Av. Siempre Viva 123"
+// Declaracion de una variable
+const nombre = "Gabriel"
+const cbu = "123456789"
+const dni = 12345678
+const telefonoCasa = "1234556768"
+const telefonoCelularUsuario = "1234556768"
+const telefono_celular_usuario = "1234556768"
+const TelefonoCelularUsuario = "1234556768"
+// const cbu = "987654321" // Error
+const CBU = "987654321"
+// const telefono-celular-usuario = "1234556768" // Error
+
+// nombre = "Juan" // Error
+// cbu = "987654321" // Error
+// dni = 87654321 // Error
+
+// Nomenclatura de variables
+// camelCase => nombreDeVariable => Mas usada
+// snake_case => nombre_de_variable
+// PascalCase => NombreDeVariable
+// kebab-case => nombre-de-variable => Error, pero sirve para nombrar archivos
+
+// JAVASCRIPT ES SENSIBLE A MAYUSCULAS Y MINUSCULAS
+// nombre != Nombre != NOMBRE
+
+// Tipos de datos
+// String => Cadena de caracteres => "Gabriel" 'Gabriel' `Gabriel`
+// Number => Numero => 123 123.45
+// Boolean => true false
+
+// String
+const string = "Luka" // => Comillas dobles
+// const string2 = 'Luka' // => Comillas simples
+const string3 = `Luka` // => Comillas invertidas
+const frase = "Luka es el profesor y esta dando clases de Javascript"
+const stringNumero = "100" // => String => No voy a poder hacer operaciones matematicas
+
+// Number
+const numeroEntero = 100 // => Numero entero => Voy a poder hacer operaciones matematicas
+const numeroDecimal = 100.5 // => Numero decimal => Voy a poder hacer operaciones matematicas
+const resultado = numeroDecimal * 2
+const resultado2 = stringNumero + stringNumero // => Concatenacion de strings
+
+// Boolean => Solo puede tener dos valores => true o false
+const verdadero = true // => true => Verdadero
+const falso = false // => false => Falso
